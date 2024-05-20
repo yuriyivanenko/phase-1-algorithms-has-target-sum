9594686b50b3dc9bad04043dd861b032853dedc3
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-20 13:56:23 -0400

    Algo to check for target sum

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let sum = false
+  for(let i = 0; i < array.length; i++){
+      for(let j = i + 1; j < array.length; j++){
+          if(array[i] + array[j] === target){
+            sum = true
+          }
+      }
+  }
+  return sum
 }
 
+// Algo cycle:  11
 /* 
   Write the Big O time complexity of your function here
+  O(n^2) quadratic
 */
 
 /* 
   Add your pseudocode here
+  Write a nested loop:
+    Outer loop:
+      Takes nth element in the array, checks to make sure it is qual to or lower than target
+    Inner loop:
+      Takes the NEXT nth element in the array, checks to make sure it is qual to or lower than target
+      Adds this number to the one from the outer loop
+      Check if the sum is equal to our target
+      Return true or false
 */
 
 /*
   Add written explanation of your solution here
+  Reminds me of the math game 24. We need to check the sum of every pair
+  of numbers to see if it is equal to the target
 */
 
 // You can run `node index.js` to view these console logs
@@ -32,3 +52,4 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10))
